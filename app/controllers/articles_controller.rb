099a182f26f5f3693dab8d43062c085b546955d1
homarinn <homarinn1230@gmail.com>
2019-07-10 19:37:49 +0900

    Add pagenate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.includes(:user)
+    @articles = Article.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def show
